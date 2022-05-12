965c131ac124b8ee34e0508b826d952a3c9de757
Rahul Kumar <irahulkv@gmail.com>
2022-05-12 15:35:15 +0530

    server code

---
+++ gameplay/sendsocket.js
@@ -0,0 +1,20 @@
+const SendSocketToSvenUP = require("./Sevenup").GetSocket;
+const SendSocketToDvT = require("./DragonVsTiger").GetSocket;
+const SendSocketToTitali = require("./TitaliGame").GetSocket;
+const SendSocketToLucky = require("./LuckyBall").GetSocket;
+
+
+const SendSocketToAndarbhar = require("./Andarbhar").GetSocket;
+
+
+
+function sendSocket(socket){
+    SendSocketToSvenUP(socket)
+    SendSocketToDvT(socket)
+    SendSocketToAndarbhar(socket)
+ SendSocketToTitali(socket)
+ SendSocketToLucky(socket)
+
+}
+
+module.exports.sendSocket = sendSocket;
